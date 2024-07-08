7bc58c37064f07b9b1b9e29be2dafc349ebf65ae
VladIsLove <f.vlad.is.love@gmail.com>
2024-07-08 19:01:22 +0300

    Changed clear Fabric code to Polymer (non-working)

--- src/main/java/com/elis/roshen/Roshen.java
+++ src/main/java/com/elis/roshen/Roshen.java
@@ -2,20 +2,30 @@ package com.elis.roshen;
 
 import com.elis.roshen.block.ModBlocks;
 import com.elis.roshen.item.ModItems;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Roshen implements ModInitializer {
-	public final static String MOD_ID = "roshen";
+    public final static String MOD_ID = "roshen";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Mod initialing");
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Mod initialing");
 
-		ModItems.registerModItems();
-		ModBlocks.registerModBlocks();
-	}
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
+
+        if (PolymerResourcePackUtils.addModAssets(MOD_ID)) {
+            LOGGER.info("Successfully added mod assets for " + MOD_ID);
+        } else {
+			LOGGER.info("Failed to add mod assets " + MOD_ID);
+        }
+
+		PolymerResourcePackUtils.markAsRequired();
+    }
 }
